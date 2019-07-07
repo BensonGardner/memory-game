e83c7bbb5e8a3f46c63d67d585baae724895a550
bensongardner <bensongardner@yahoo.com>
2019-07-07 01:00:36 -0500

    docs: Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+#Another Memory Game
+By Benson Gardner
+
+##About
+This game was written by Benson Gardner to complete the requirements of the Udacity "Memory Game" project. 
+
+This project was created from scratch, without any "starter code."
+
+##Elements
+The game consists of index.html, style.css, and game.js. After loading the index, selecting a card starts the game.
--- game.js
+++ game.js
@@ -21,7 +21,11 @@ let num = 0,
     newStar = null,
     symbol = '',
     cardHTML = '',
-    endGameInfo = 0;
+    endGameInfo = 0,
+    messages = document.createElement('div'),
+    messageBox = document.createElement('div'),
+    mainMessage = document.createElement('p'),
+    bodyHeight = '';  
 
 // To shuffle the deck, first we create an
 // array of randomly ordered numbers, 1-12.
@@ -32,12 +36,12 @@ function shuffle() {
         num = Math.round(Math.random() * 11);
         if (!cardPositions.includes(num)) {
             cardPositions.push(num);
-        };
+        }
         shuffle();
     } else {
         return;
     }
-};
+}
     
 function deal() {
     symbol = '';
@@ -56,10 +60,10 @@ function deal() {
     };
     
     const cards = document.querySelectorAll('.card');
-    for (card of cards) {
+    for (let card of cards) {
         card.addEventListener('click', flip);
     };
-};
+}
 
 function countdown() {
     if (timer === 0) {
@@ -71,23 +75,23 @@ function countdown() {
         document.querySelector('#time').innerHTML = timer/1000;
     } else {
         return;
-    };
-};
+    }
+}
 
 function drawStars() {
     document.querySelector('#stars').innerHTML = '';
 
     // Draw either a star or the outline of a star in the
     // rating section of the page. 
-    for (i = 0; i < 3; i++) {
+    for (let i = 0; i < 3; i++) {
         if (i < starCount) {
             newStar = star.cloneNode(true);
         } else {
             newStar = starOutline.cloneNode(true);
-        };
+        }
         document.querySelector('#stars').appendChild(newStar);
     }
-};
+}
 
 function advanceMoves() {
     moves++;
@@ -95,10 +99,10 @@ function advanceMoves() {
     if (moves === 9 || moves === 14 || moves === 17) {
         starCount--;
         drawStars();
-    };
+    }
     if (starCount == 0) {
         lose("Out of moves!");
-    };
+    }
 }
 
 function flip(e) {
@@ -111,14 +115,14 @@ function flip(e) {
     } else if (gameState === 'waiting') {
         gameState = 'running';
         setTimeout(countdown, 1000);
-    };
+    }
     
     let currentCard = e.target; 
     
     // Don't allow a face-up card to be clicked
     if (currentCard.classList.contains('face-up') || document.getElementsByClassName('face-up').length > 1) {
         return;
-    };
+    }
     
     // Otherwise, flip the card from face-down to face-up
     currentCard.classList.remove('face-down');
@@ -141,9 +145,9 @@ function flip(e) {
             mismatch();
         } else {
             faceUpSymbols.push(symbol);
-        };
+        }
     }
-};
+}
 
 function match() {
     
@@ -151,16 +155,16 @@ function match() {
     // so they're clearly not in play
     let faceUpCards = document.querySelectorAll('.face-up');
     setTimeout(function() {
-        for (faceUpCard of faceUpCards) {
+        for (let faceUpCard of faceUpCards) {
             faceUpCard.classList.add('matched');
             faceUpCard.classList.remove('face-up');
-        }
+        };
         faceUpSymbols = [];
         if (document.querySelectorAll('.face-down').length === 0) {
             win();
-        };
+        }
     }, 250);
-};
+}
 
 function mismatch() {
 
@@ -168,13 +172,13 @@ function mismatch() {
     // pair back to face-down
     setTimeout(function() {
         let revealedCards = document.querySelectorAll('.face-up');
-        for (revealedCard of revealedCards) {
+        for (let revealedCard of revealedCards) {
             revealedCard.classList.add('face-down');
             revealedCard.classList.remove('face-up');
         };
     }, 1000);
     faceUpSymbols = [];
-};
+}
 
 function modal(condition, message) {
     
@@ -185,16 +189,13 @@ function modal(condition, message) {
     document.querySelector('#board').prepend(screenDarkener);
 
     // Create the modal with customized message
-    let messages = document.createElement('div'),
-        messageBox = document.createElement('div'),
-        mainMessage = document.createElement('p');
     messageBox.innerHTML = '<button type="button" id="reset2">New Game</button>';
     mainMessage.innerHTML = message;
     if (condition === 'win') {
         messages.innerHTML = '<p>Your time: ' + 
             (35000 - timer)/1000 + ' seconds</p><p>Your rating: ' +
             document.querySelector('#stars').innerHTML + '</p>';
-    };
+    }
     messageBox.prepend(messages);
     messageBox.prepend(mainMessage);
 
@@ -208,7 +209,7 @@ function modal(condition, message) {
 function win() {
     gameState = 'won';
     modal('win', 'Congratulations! You won!');
-};
+}
 
 function lose(reason) {
     gameState = 'lost';
@@ -219,12 +220,12 @@ function lose(reason) {
 function startGame() {
     
     // Remove the modal and screen darkener if present
-    if (typeof messageBox !== 'undefined') {
+    if (document.querySelector('#messageBox')) {
         document.querySelector('#messageBox').remove();
-    };
+    }
     if (screenDarkener.parentElement === document.body) {
         document.body.removeChild(screenDarkener);
-    };
+    }
     
     // Set values and board state back to starting positions
     gameState = 'waiting';
@@ -243,7 +244,6 @@ function startGame() {
     deal();
     drawStars();
     document.querySelector('#reset').addEventListener('click', startGame);
-};
+}
 
 startGame();
-
